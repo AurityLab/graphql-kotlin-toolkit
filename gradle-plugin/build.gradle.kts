16161be0f7c35190a1b19b519bb5b5e09ac7ab94
Andre Loreth <andre@loreth.co>
2020-01-04 18:13:03 +0100

    feat(gradle-plugin): add generated source set

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
     implementation(gradleApi())
     implementation(project(":codegen"))
 }
--- gradle-plugin/src/main/kotlin/com/auritylab/gql/kotlin/gradle/PoetGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/auritylab/gql/kotlin/gradle/PoetGradlePlugin.kt
@@ -4,7 +4,11 @@ import com.auritylab.gql.kotlin.gradle.extension.GenerateExtension
 import com.auritylab.gql.kotlin.gradle.task.GenerateTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.create
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class PoetGradlePlugin : Plugin<Project> {
     companion object {
@@ -14,8 +18,25 @@ class PoetGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.run {
             val generateExtension = extensions.create<GenerateExtension>("graphqlKotlinGenerate")
+            val defaultOutputDirectory = project.layout.buildDirectory.dir("generated/graphql/kotlin/main/")
 
-            generateExtension.outputDirectory.set(project.layout.buildDirectory.dir("generate-resources/main"))
+            generateExtension.outputDirectory.set(defaultOutputDirectory)
+
+            /*the<JavaPluginConvention>().sourceSets {
+                "main" {
+                    java {
+                        srcDirs(defaultOutputDirectory)
+                    }
+                }
+            }*/
+
+            this.afterEvaluate {
+                the<KotlinJvmProjectExtension>().sourceSets {
+                    "main" {
+                        kotlin.srcDir(defaultOutputDirectory)
+                    }
+                }
+            }
 
             tasks.apply {
                 create("graphqlKotlinGenerate", GenerateTask::class) {
