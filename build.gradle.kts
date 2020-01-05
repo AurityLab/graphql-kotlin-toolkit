ff3b406957d451687121e3cfd4cf612f862db10f
Andre Loreth <andre@loreth.co>
2020-01-05 11:16:10 +0100

    chore: add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50" apply false
     id("org.gradle.kotlin.kotlin-dsl") version "1.3.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
 }
 
 subprojects {
     group = "com.auritylab.graphql-kotlin-toolkit"
     version = "0.1.0"
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     repositories {
         jcenter()
