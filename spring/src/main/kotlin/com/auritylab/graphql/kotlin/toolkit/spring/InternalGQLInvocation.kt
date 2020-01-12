bce0ce240039dafeb4cb3f36e16be9ce67d33b15
Andre Loreth <andre@loreth.co>
2020-01-12 12:24:02 +0100

    feat(spring): implement InternalGQLInvocation

--- spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/InternalGQLInvocation.kt
+++ spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/InternalGQLInvocation.kt
@@ -1,19 +1,25 @@
 package com.auritylab.graphql.kotlin.toolkit.spring
 
 import com.auritylab.graphql.kotlin.toolkit.spring.api.GQLInvocation
+import graphql.ExecutionInput
 import graphql.ExecutionResult
 import graphql.GraphQL
-import java.util.concurrent.CompletableFuture
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.WebRequest
+import java.util.concurrent.CompletableFuture
 
 @Component
 @ConditionalOnMissingBean(GQLInvocation::class)
-internal class DefaultGQLInvocation(
+internal class InternalGQLInvocation(
     private val gql: GraphQL
 ) : GQLInvocation {
     override fun invoke(data: GQLInvocation.Data, request: WebRequest): CompletableFuture<ExecutionResult> {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+        val executionInput = ExecutionInput.newExecutionInput()
+            .query(data.query)
+            .operationName(data.operationName)
+            .variables(data.variables)
+            .build()
+        return gql.executeAsync(executionInput)
     }
 }
