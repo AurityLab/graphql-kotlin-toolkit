8fd1ee5298d989c38b8b018af72a35ccac62ce46
Andre Loreth <andre@loreth.co>
2020-01-12 10:39:09 +0100

    chore: fix codestyle

--- spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/DefaultGQLInvocation.kt
+++ spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/DefaultGQLInvocation.kt
@@ -3,10 +3,10 @@ package com.auritylab.graphql.kotlin.toolkit.spring
 import com.auritylab.graphql.kotlin.toolkit.spring.api.GQLInvocation
 import graphql.ExecutionResult
 import graphql.GraphQL
+import java.util.concurrent.CompletableFuture
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.WebRequest
-import java.util.concurrent.CompletableFuture
 
 @Component
 @ConditionalOnMissingBean(GQLInvocation::class)
@@ -14,6 +14,6 @@ internal class DefaultGQLInvocation(
     private val gql: GraphQL
 ) : GQLInvocation {
     override fun invoke(data: GQLInvocation.Data, request: WebRequest): CompletableFuture<ExecutionResult> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
--- spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/GQLController.kt
+++ spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/GQLController.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import graphql.ExecutionResult
+import java.util.concurrent.CompletableFuture
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.server.ResponseStatusException
-import java.util.concurrent.CompletableFuture
 
 @Component
 internal class GQLController(
