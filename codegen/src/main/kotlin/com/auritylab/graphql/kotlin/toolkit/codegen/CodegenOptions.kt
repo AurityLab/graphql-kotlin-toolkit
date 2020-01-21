5b3e3c738a315da25aa88620ccfb61de911e3679
Andre Loreth <andre.loreth@live.de>
2020-01-21 18:51:54 +0100

    feat(codegen): CodegenOptions: add comments

--- codegen/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/codegen/CodegenOptions.kt
+++ codegen/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/codegen/CodegenOptions.kt
@@ -2,10 +2,35 @@ package com.auritylab.graphql.kotlin.toolkit.codegen
 
 import java.nio.file.Path
 
+/**
+ * Describes configurable options for the code generator.
+ */
 data class CodegenOptions(
+    /**
+     * Describes a [Collection] of [Path]s which point to schemas.
+     * This property is necessary!
+     */
     val schemas: Collection<Path>,
+
+    /**
+     * Describes a [Path] which acts as output directory for the generated code.
+     */
     val outputDirectory: Path,
+
+    /**
+     * Describes a global prefix for all generated files and classes/enums/etc.
+     * Defaults to `null` (no prefix).
+     */
     var generatedGlobalPrefix: String? = null,
+
+    /**
+     * Describes the name of the packages which contains all generated code.
+     */
     var generatedBasePackage: String = "graphql.kotlin.toolkit.codegen",
+
+    /**
+     * Describes if the code shall be generated for all found types, enums, etc.
+     * If this is [true] it will ignore the generate directive.
+     */
     var generateAll: Boolean = true
 )
