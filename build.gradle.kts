0f48ed3a5ed7f5c251259e1ff063370a45309cfd
Andre Loreth <andre.loreth@live.de>
2020-01-23 20:45:00 +0100

    fix: add kotlin plugin spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50" apply false
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50" apply false
     id("org.gradle.kotlin.kotlin-dsl") version "1.3.1" apply false
     id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
 }
