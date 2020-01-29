032774d06e200ed868903fa7c5dfac4da9e1b2ae
Andre Loreth <andre.loreth@live.de>
2020-01-29 19:49:27 +0100

    chore(spring): add comments to GQLInvocation

--- graphql-kotlin-toolkit-spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/api/GQLInvocation.kt
+++ graphql-kotlin-toolkit-spring/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/api/GQLInvocation.kt
@@ -1,12 +1,20 @@
 package com.auritylab.graphql.kotlin.toolkit.spring.api
 
 import graphql.ExecutionResult
-import java.util.concurrent.CompletableFuture
 import org.springframework.web.context.request.WebRequest
+import java.util.concurrent.CompletableFuture
 
 interface GQLInvocation {
+    /**
+     * Will execute the GraphQL Query (described in [data]) and return a [CompletableFuture]
+     * which contains the [ExecutionResult]. The corresponding [WebRequest] is also supplied
+     * to access additional information
+     */
     fun invoke(data: Data, request: WebRequest): CompletableFuture<ExecutionResult>
 
+    /**
+     * Describes the GraphQL Query.
+     */
     data class Data(
         val query: String,
         val operationName: String?,
