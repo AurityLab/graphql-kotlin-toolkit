0703d788c66e395c339edea96677a50ed8dff3ce
Andre Loreth <andre@loreth.co>
2020-02-07 22:30:00 +0100

    chore(spring-boot): simplify method

--- graphql-kotlin-toolkit-spring-boot/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/InternalGQLInvocation.kt
+++ graphql-kotlin-toolkit-spring-boot/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/InternalGQLInvocation.kt
@@ -4,22 +4,20 @@ import com.auritylab.graphql.kotlin.toolkit.spring.api.GraphQLInvocation
 import graphql.ExecutionInput
 import graphql.ExecutionResult
 import graphql.GraphQL
-import java.util.concurrent.CompletableFuture
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.WebRequest
+import java.util.concurrent.CompletableFuture
 
 @Component
-@ConditionalOnMissingBean(GraphQLInvocation::class)
 internal class InternalGQLInvocation(
     private val gql: GraphQL
 ) : GraphQLInvocation {
-    override fun invoke(data: GraphQLInvocation.Data, request: WebRequest): CompletableFuture<ExecutionResult> {
-        val executionInput = ExecutionInput.newExecutionInput()
-            .query(data.query)
-            .operationName(data.operationName)
-            .variables(data.variables)
-            .build()
-        return gql.executeAsync(executionInput)
-    }
+    override fun invoke(data: GraphQLInvocation.Data, request: WebRequest): CompletableFuture<ExecutionResult> =
+        gql.executeAsync(
+            ExecutionInput.newExecutionInput()
+                .query(data.query)
+                .operationName(data.operationName)
+                .variables(data.variables ?: mapOf())
+                .build()
+        )
 }
