6cd30012d8e264e57112734426ca41680241c21e
Andre Loreth <andre@loreth.co>
2020-02-12 19:11:14 +0100

    chore: fix codestyle

--- graphql-kotlin-toolkit-spring-boot/src/test/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/ControllerMockedInvocationTest.kt
+++ graphql-kotlin-toolkit-spring-boot/src/test/kotlin/com/auritylab/graphql/kotlin/toolkit/spring/ControllerMockedInvocationTest.kt
@@ -8,6 +8,7 @@ import com.nhaarman.mockitokotlin2.check
 import com.nhaarman.mockitokotlin2.reset
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import java.util.UUID
 import me.lazmaid.kraph.Kraph
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
@@ -24,7 +25,6 @@ import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.multipart
 import org.springframework.test.web.servlet.post
 import org.springframework.web.multipart.MultipartFile
-import java.util.UUID
 
 @DirtiesContext
 @AutoConfigureMockMvc
