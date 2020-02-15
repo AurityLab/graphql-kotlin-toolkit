7e86d9937aafffb5e0bf9859491734bdaafd78f9
Andre Loreth <andre@loreth.co>
2020-02-15 12:40:54 +0100

    chore: bump to v0.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ allprojects {
 
 subprojects {
     group = "com.auritylab.graphql-kotlin-toolkit"
-    version = "0.2.0"
+    version = "0.2.1"
 
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
