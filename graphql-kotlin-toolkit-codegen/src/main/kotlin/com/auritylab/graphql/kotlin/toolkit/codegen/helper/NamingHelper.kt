3b47ce872878b5086a5521cc52167547bacf2460
Andre Loreth <andre.loreth@live.de>
2020-04-25 10:16:04 +0200

    feat: NamingHelper: implement lowercaseFirstLetter and add extension functions

--- graphql-kotlin-toolkit-codegen/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/codegen/helper/NamingHelper.kt
+++ graphql-kotlin-toolkit-codegen/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/codegen/helper/NamingHelper.kt
@@ -1,10 +1,37 @@
 package com.auritylab.graphql.kotlin.toolkit.codegen.helper
 
+/**
+ * A global helper object which provides some common used methods to work with strings.
+ */
 internal object NamingHelper {
     /**
-     * Will uppercase the first letter of the given [string].
-     * If the given [string] is `getUser` this method will return `GetUser`.
+     * Will uppercase the first letter of the given [String]. To be more precise, if [i] is "getUser", this method
+     * will return "GetUser".
+     *
+     * @param i The string which shall be transformed.
+     * @return The transformed string.
      */
-    fun uppercaseFirstLetter(string: String): String =
-            (string.substring(0, 1).toUpperCase()) + string.substring(1)
+    fun uppercaseFirstLetter(i: String): String = (i.substring(0, 1).toUpperCase()) + i.substring(1)
+
+    /**
+     * Will lowercase the first letter for the given [String]. To be more precise, if [i] is "GetUser", this method
+     * will return "getUser"
+     * @param i The string which shall be transformed.
+     * @return The transformed string.
+     */
+    fun lowercaseFirstLetter(i: String): String = (i.substring(0, 1).toLowerCase()) + i.substring(1)
 }
+
+/**
+ * Extension function which delegates to [NamingHelper.uppercaseFirstLetter].
+ *
+ * @see NamingHelper.uppercaseFirstLetter
+ */
+internal fun String.uppercaseFirst(): String = NamingHelper.uppercaseFirstLetter(this)
+
+/**
+ * Extension function which delegates to [NamingHelper.lowercaseFirstLetter].
+ *
+ * @see NamingHelper.lowercaseFirstLetter
+ */
+internal fun String.lowercaseFirst(): String = NamingHelper.lowercaseFirstLetter(this)
