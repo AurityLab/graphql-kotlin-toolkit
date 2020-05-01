2ab7b55ad24c0a4a8377ce2274078ef9fd5fd67f
Andre Loreth <andre.loreth@live.de>
2020-05-01 10:39:52 +0200

    chore: fix typo in comment

--- graphql-kotlin-toolkit-common/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/common/directive/HasArgumentsDirective.kt
+++ graphql-kotlin-toolkit-common/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/common/directive/HasArgumentsDirective.kt
@@ -8,7 +8,7 @@ import graphql.schema.GraphQLDirectiveContainer
  */
 interface HasArgumentsDirective<M : Any> : Directive {
     /**
-     * Will resolve the given [directive] into an ínstance of [M], which is a representation of the arguments.
+     * Will resolve the given [directive] into an instance of [M], which is a representation of the arguments.
      */
     fun getArguments(directive: GraphQLDirective): M
 
