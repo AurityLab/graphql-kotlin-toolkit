98f1f29f0009e714aa72474686b148f845685183
Andre Loreth <andre@loreth.co>
2020-09-26 12:53:56 +0200

    chore: refactored buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+version = "0.6.2"
+group = "com.auritylab.graphql-kotlin-toolkit"
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.10" apply false
     id("org.jetbrains.kotlin.plugin.spring") version "1.4.10" apply false
@@ -12,13 +15,14 @@ allprojects {
 }
 
 subprojects {
-    group = "com.auritylab.graphql-kotlin-toolkit"
-    version = "0.6.2"
+    group = parent!!.group
+    version = parent!!.version
 
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     dependencies {
         "implementation"(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -43,6 +47,95 @@ subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             jvmTarget = "1.8"
+        }
+    }
+
+    configure<SigningExtension> {
+        the<PublishingExtension>().publications
+            .findByName("maven")
+            ?.let { sign(it) }
+    }
+
+    tasks.create("sourceJar", Jar::class) {
+        archiveClassifier.set("sources")
+        doLast {
+            from(the<SourceSetContainer>().getByName("main").allSource)
+        }
+    }
+
+    tasks.create("javadocJar", Jar::class) {
+        group = JavaBasePlugin.DOCUMENTATION_GROUP
+        description = "Assembles Kotlin docs with Dokka"
+        archiveClassifier.set("javadoc")
+        from(tasks.getByName("dokka"))
+    }
+
+    afterEvaluate {
+        // Check if publication shall be configured for this project.
+        val publicationEnabled = ext.properties["publication.enabled"]?.let { it as? Boolean } ?: false
+        val publicationArtifactId = ext.properties["publication.artifactId"]?.let { it as? String } ?: ""
+        val publicationName = ext.properties["publication.name"]?.let { it as? String } ?: ""
+        val publicationDescription = ext.properties["publication.description"]?.let { it as? String } ?: ""
+
+        if (publicationEnabled) {
+            configure<PublishingExtension> {
+                publications {
+                    create<MavenPublication>("maven") {
+                        artifactId = publicationArtifactId
+                        from(components.getByName("java"))
+                        artifact(tasks.getByName("sourceJar"))
+                        artifact(tasks.getByName("javadocJar"))
+
+                        repositories {
+                            maven {
+                                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                                credentials {
+                                    username = if (project.hasProperty("ossrhUsername"))
+                                        project.property("ossrhUsername") as String else null
+                                    password = if (project.hasProperty("ossrhPassword"))
+                                        project.property("ossrhPassword") as String else null
+                                }
+                            }
+                        }
+
+                        pom {
+                            name.set(publicationName)
+                            description.set(publicationDescription)
+                            url.set("https://github.com/AurityLab/graphql-kotlin-toolkit")
+
+                            organization {
+                                name.set("AurityLab UG (haftungsbeschraenkt)")
+                                url.set("https://github.com/AurityLab")
+                            }
+
+                            issueManagement {
+                                system.set("GitHub")
+                                url.set("https://github.com/AurityLab/graphql-kotlin-toolkit/issues")
+                            }
+
+                            licenses {
+                                license {
+                                    name.set("Apache License 2.0")
+                                    url.set("https://github.com/AurityLab/graphql-kotlin-toolkit/blob/master/LICENSE")
+                                    distribution.set("repo")
+                                }
+                            }
+
+                            scm {
+                                url.set("https://github.com/AurityLab/graphql-kotlin-toolkit")
+                                connection.set("scm:git:git://github.com/AurityLab/graphql-kotlin-toolkit.git")
+                                developerConnection.set("scm:git:ssh://git@github.com:AurityLab/graphql-kotlin-toolkit.git")
+                            }
+
+                            developers {
+                                developer {
+                                    id.set("WipeAir")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- graphql-kotlin-toolkit-spring-boot/build.gradle.kts
+++ graphql-kotlin-toolkit-spring-boot/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     id("org.jetbrains.kotlin.kapt")
-    id("signing")
     id("org.jetbrains.kotlin.plugin.spring")
+}
+
+ext {
+    this["publication.enabled"] = true
+    this["publication.artifactId"] = "spring-boot"
+    this["publication.name"] = "GraphQL Kotlin Toolkit: Spring"
+    this["publication.description"] = "GraphQL integration for Spring"
 }
 
 dependencies {
@@ -26,79 +32,4 @@ dependencies {
 
     testImplementation("me.lazmaid.kraph:kraph:0.6.1")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-}
-
-tasks.create("sourceJar", Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
-}
-
-tasks.create("javadocJar", Jar::class) {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    description = "Assembles Kotlin docs with Dokka"
-    archiveClassifier.set("javadoc")
-    from(tasks.dokka)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = "spring-boot"
-            from(components["java"])
-            artifact(tasks.getByName("sourceJar"))
-            artifact(tasks.getByName("javadocJar"))
-
-            repositories {
-                maven {
-                    url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-                    credentials {
-                        username = if (project.hasProperty("ossrhUsername"))
-                            project.property("ossrhUsername") as String else null
-                        password = if (project.hasProperty("ossrhPassword"))
-                            project.property("ossrhPassword") as String else null
-                    }
-                }
-            }
-
-            pom {
-                name.set("GraphQL Kotlin Toolkit: Spring")
-                description.set("GraphQL integration for Spring")
-                url.set("https://github.com/AurityLab/graphql-kotlin-toolkit")
-
-                organization {
-                    name.set("AurityLab UG (haftungsbeschraenkt)")
-                    url.set("https://github.com/AurityLab")
-                }
-
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("https://github.com/AurityLab/graphql-kotlin-toolkit/issues")
-                }
-
-                licenses {
-                    license {
-                        name.set("Apache License 2.0")
-                        url.set("https://github.com/AurityLab/graphql-kotlin-toolkit/blob/master/LICENSE")
-                        distribution.set("repo")
-                    }
-                }
-
-                scm {
-                    url.set("https://github.com/AurityLab/graphql-kotlin-toolkit")
-                    connection.set("scm:git:git://github.com/AurityLab/graphql-kotlin-toolkit.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:AurityLab/graphql-kotlin-toolkit.git")
-                }
-
-                developers {
-                    developer {
-                        id.set("WipeAir")
-                    }
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications.getByName("maven"))
 }
