3047e5b24e6b7ff20518f780c1bcf67bd90dac30
Andre Loreth <andre@loreth.co>
2021-05-17 20:32:11 +0200

    chore: add Experimental annotation for experimental public AP

---
+++ graphql-kotlin-toolkit-common/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/common/markers/Experimental.kt
@@ -0,0 +1,7 @@
+package com.auritylab.graphql.kotlin.toolkit.common.markers
+
+/**
+ * Marker for a public API which is in experimental state as it might change in the future.
+ */
+@Retention(AnnotationRetention.SOURCE)
+annotation class Experimental()
