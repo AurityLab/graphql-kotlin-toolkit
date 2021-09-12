b6b0c1d358574861aa2e32177dcd69edea7e541a
Andre Loreth <andre@loreth.co>
2021-09-12 14:15:50 +0200

    fix: delete unused exception

--- graphql-kotlin-toolkit-common/src/main/kotlin/com/auritylab/graphql/kotlin/toolkit/common/directive/exception/DirectiveNotFoundException.kt
+++
@@ -1,9 +0,0 @@
-package com.auritylab.graphql.kotlin.toolkit.common.directive.exception
-
-import com.auritylab.graphql.kotlin.toolkit.common.directive.Directive
-
-/**
- * Represents a exception, which will be thrown when the directive was not found on a container.
- */
-class DirectiveNotFoundException(directive: Directive, message: String) :
-    Exception("Directive '${directive.name}': $message")
